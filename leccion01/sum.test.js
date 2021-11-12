4a5e4bc103f6332941ec92685bdebeaec9aea2f2
Horacio Herrera <hi@horacioh.com>
2021-11-12 23:38:03 +0100

    fix lesson 01

--- leccion01/sum.test.js
+++ leccion01/sum.test.js
@@ -1,4 +1,4 @@
-const {Object} = require("./custom-object")
+const { sum } = require("./sum")
 
 describe("sum", () => {
   test("sum(2, 1)", () => {
@@ -18,20 +18,20 @@ describe("sum", () => {
   })
 
   test(`sum(null, 1)`, () => {
-    expect(sum(null, 1)).toEqual('invalid')
+    expect(sum(null, 1)).toEqual("invalid")
   })
 
   test(`sum(2)`, () => {
-    expect(sum(2)).toEqual('invalid')
+    expect(sum(2)).toEqual("invalid")
   })
 
   test(`sum("2", [])`, () => {
-    expect(sum('2', [])).toEqual('invalid')
+    expect(sum("2", [])).toEqual("invalid")
   })
 })
 
-describe('sum Extra Points', () => {
+describe("sum Extra Points", () => {
   test.skip(`sum(2, NaN)`, () => {
-    expect(sum(2, NaN)).toEqual('invalid')
+    expect(sum(2, NaN)).toEqual("invalid")
   })
 })
